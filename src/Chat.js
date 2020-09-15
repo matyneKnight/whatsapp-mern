0a3413c17856bef6c4c6879a4e9b2131fd5048e4
Abdel Matyne LAWANI <matyne.lawani@bennu.tech>
2020-09-15 16:16:03 +0000

    Add chat input for sender

--- src/Chat.js
+++ src/Chat.js
@@ -1,7 +1,7 @@
-import { Avatar, IconButton } from '@material-ui/core';
-import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
 import React from 'react';
 import './Chat.css';
+import { Avatar, IconButton } from '@material-ui/core';
+import { AttachFile, InsertEmoticon, Mic, MoreVert, SearchOutlined } from '@material-ui/icons';
 
 function Chat() {
   return (
@@ -37,6 +37,15 @@ function Chat() {
           This is a message
           <span className="chat__timestamp">{new Date().toUTCString()}</span>
         </p>
+      </div>
+
+      <div className="chat__footer">
+        <InsertEmoticon />
+        <form>
+          <input type="text" placeholder="Type a message..."/>
+          <button type="submit">Send a message</button>
+        </form>
+        <Mic />
       </div>
     </div>
   );
