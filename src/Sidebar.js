e8ab731837dea294dc19bf334982066d2136cadb
Abdel Matyne LAWANI <matyne.lawani@bennu.tech>
2020-09-15 14:45:36 +0000

    Add sidebar head, search container

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import "./Sidebar.css";
+import { Chat, DonutLarge, MoreVert, SearchOutlined } from '@material-ui/icons';
+import { Avatar, IconButton } from '@material-ui/core';
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <h1>I am the sidebar</h1>
+      <div className="sidebar__header">
+        <Avatar src="https://avatars1.githubusercontent.com/u/44756140?s=400&u=5378afea8dc7a60522a0970d84753d0f2b9a4148&v=4" />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLarge />
+          </IconButton>
+          <IconButton>
+            <Chat />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlined />
+          <input type="text" placeholder="Search or start new chat"/>
+        </div>
+      </div>
     </div>
   )
 }
