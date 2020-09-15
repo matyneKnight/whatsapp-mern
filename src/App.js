37dc53a7633b8f0c3ca8440f32f928dac8e89aa8
Abdel Matyne LAWANI <matyne.lawani@bennu.tech>
2020-09-15 14:09:04 +0000

    init sidebar and chat components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Chat from './Chat';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Let's build a whatsapp clone mern stack</h1>
+    <div className="app">
+      <div className="app__body">
+        <Sidebar />
+        <Chat  />
+      </div>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCKKrQwtfIyzzTeTohPgWiCilvnz2DW5VU",
+  authDomain: "whatsapp-mern-4221b.firebaseapp.com",
+  databaseURL: "https://whatsapp-mern-4221b.firebaseio.com",
+  projectId: "whatsapp-mern-4221b",
+  storageBucket: "whatsapp-mern-4221b.appspot.com",
+  messagingSenderId: "110655835315",
+  appId: "1:110655835315:web:d439ccbf4480718cff74c6"
+};
