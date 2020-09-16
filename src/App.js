1f3de5cb4a2fa9536659ff0a8f868bf4acc67388
Abdel Matyne LAWANI <matyne.lawani@bennu.tech>
2020-09-16 08:43:59 +0000

    Add pusher messages channel subscribing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Pusher from 'pusher-js'
 import './App.css';
 import Chat from './Chat';
 import Sidebar from './Sidebar';
 
 function App() {
+
+  useEffect(() => {
+    const pusher = new Pusher('77590d34eeb84fd58081', {
+      cluster: 'mt1'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('insert', (data) => {
+      alert(JSON.stringify(data));
+    });
+
+  }, [])
+
   return (
     <div className="app">
       <div className="app__body">
